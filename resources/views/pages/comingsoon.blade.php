ea15fed9694cea75668c59e8c30214f0e1d465f0
Matheus Ronan Abel <94665822+matheusronanabel@users.noreply.github.com>
2022-11-30 16:30:52 +0800

    Added Tailwindcss and Fix Laravel Mix

---
+++ resources/views/pages/comingsoon.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.comingsoon-layout')
+
+@section('pageTitle', 'Coming Soon')
+
+@section('content')
+    <section class="w-full h-screen flex items-center justify-center">
+        <div class="grid grid-cols-12">
+            <div class="col-span-12">
+                <h2 class="text-white">
+                    Coming Soon
+                </h2>
+                <h1 class="text-white font-black tracking-tight text-6xl">
+                    Bali Beauty Center
+                </h1>
+            </div>
+        </div>
+
+    </section>
+@endsection
